f0189917f632ee427ed95fe8e403419f76e481c2
Md Sabbir Hossain <sabbir15-2809@diu.edu.bd>
2025-04-27 22:26:12 +0600

    update: env

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,9 +3,10 @@ export const AUTH_USER_KEY = import.meta.env.VITE_AUTH_USER_KEY!;
 export const USER_TYPE = import.meta.env.VITE_AUTH_USER_TYPE!;
 
 export const FRONTEND_BASE_URL = import.meta.env.VITE_FRONTEND_BASE_URL!;
+
 export const AUTH_BACKEND_BASE_URL = import.meta.env
   .VITE_AUTH_BACKEND_BASE_URL!;
 export const READER_BACKEND_BASE_URL = import.meta.env
   .VITE_READER_BACKEND_BASE_URL!;
-export const WRITE_BACKEND_BASE_URL = import.meta.env
-  .VITE_WRITE_BACKEND_BASE_URL!;
+export const WRITER_BACKEND_BASE_URL = import.meta.env
+  .VITE_WRITER_BACKEND_BASE_URL!;
--- src/services/writer-api.ts
+++ src/services/writer-api.ts
@@ -1,7 +1,7 @@
-import { WRITE_BACKEND_BASE_URL } from "../constants";
+import { WRITER_BACKEND_BASE_URL } from "../constants";
 import { createAPI } from "../lib/axios/createAPI";
 
-const WRITER = createAPI(WRITE_BACKEND_BASE_URL);
+const WRITER = createAPI(WRITER_BACKEND_BASE_URL);
 
 export const updateOrder = async (orderId: string, body: object) => {
   const response = await WRITER.put(`/orders/${orderId}`, body);
